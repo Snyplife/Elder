ea44249436fb702ec0d4dfd8f02a0a8da1586e9c
Snyplife <Nassimbelferoum@gmail.com>
2017-08-29 15:55:44 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+let http = require("http");
+http.createServer(function(req, res){
+  res.writeHead(200, {
+    'Content-Type': 'text/plain '
+  });
+  res.write("Coucou Nassim1 !");
+  res.end()
+}).listen(7777);
